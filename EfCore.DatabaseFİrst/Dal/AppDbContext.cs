451c275e368fdf210986c918db073fa5727c7db9
mfthdkr <50250549+mfthdkr@users.noreply.github.com>
2022-10-02 13:08:06 +0300

    Add project files.

---
+++ EfCore.DatabaseFİrst/Dal/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace EfCore.DatabaseFirst.Dal
+{
+    public class AppDbContext: DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=EfCoreDatabaseFirstDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+    }
+}
---
+++ EfCore.DatabaseFİrst/Program.cs
@@ -0,0 +1,12 @@
+﻿using EfCore.DatabaseFirst.Dal;
+using Microsoft.EntityFrameworkCore;
+
+using (var context = new AppDbContext())
+{
+    var products = await context.Products.ToListAsync();
+
+    products.ForEach(p =>
+    {
+        Console.WriteLine($"{p.Id} : {p.Name}");
+    });
+}
